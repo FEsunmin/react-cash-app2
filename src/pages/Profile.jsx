856ee6b10639e13c9992e0a4c9c2b353330c0646
FE_sunmin <fe.sunmin@gmail.com>
2024-06-14 12:20:38 +0900

    style 수정

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,6 +3,53 @@ import { updateProfile } from "../lib/api/auth";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
+const Container = styled.div`
+  width: 500px;
+  height: 400px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  border-radius: 10px;
+  border: 1px solid skyblue;
+`;
+
+const FlexFrame = styled.div`
+  height: 200px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: column;
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 600;
+  margin-bottom: 40px;
+`;
+
+const InputFrame = styled.input`
+  width: 280px;
+  height: 25px;
+`;
+
+const Button = styled.button`
+  width: 350px;
+  height: 30px;
+  margin-top: 20px;
+  border: none;
+  border-radius: 50px;
+  background-color: skyblue;
+`;
+
+const FormFrame = styled.div`
+  width: 350px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
 const Profile = ({ user, setUser }) => {
   const [nickname, setNickname] = useState("");
   const [avatar, setAvatar] = useState(null);
@@ -25,29 +72,31 @@ const Profile = ({ user, setUser }) => {
   };
 
   return (
-    <div>
-      <h2>프로필 수정</h2>
-      <div>
-        <label htmlFor="nickname">닉네임</label>
-        <input
-          type="text"
-          onChange={(e) => setNickname(e.target.value)}
-          placeholder="닉네임"
-          minLength="1"
-          maxLength="10"
-        />
-      </div>
+    <Container>
+      <FlexFrame>
+        <Title>프로필 수정</Title>
+        <FormFrame>
+          <label htmlFor="nickname">닉네임</label>
+          <InputFrame
+            type="text"
+            onChange={(e) => setNickname(e.target.value)}
+            placeholder="닉네임"
+            minLength="1"
+            maxLength="10"
+          />
+        </FormFrame>
 
-      <div>
-        <label htmlFor="nickname">아바타 이미지</label>
-        <input
-          type="file"
-          onChange={(e) => setAvatar(e.target.files[0])}
-          accept="image/*"
-        />
-      </div>
-      <button onClick={handleUpdateProfile}>프로필 업데이트</button>
-    </div>
+        <FormFrame>
+          <label htmlFor="nickname">아바타 이미지</label>
+          <input
+            type="file"
+            onChange={(e) => setAvatar(e.target.files[0])}
+            accept="image/*"
+          />
+        </FormFrame>
+        <Button onClick={handleUpdateProfile}>프로필 업데이트</Button>
+      </FlexFrame>
+    </Container>
   );
 };
 
